3139906bda86f5172a80aa1432c64cf912eecac0
Lennon Martins <lennonmcarlos@gmail.com>
2022-05-17 17:42:49 -0400

    Selecionar todos

---
+++ js/matricula.js
@@ -0,0 +1,49 @@
+var totalCursos = document.querySelector(".js-total-de-cursos");
+var totalHoras = document.querySelector(".js-total-de-horas");
+var totalPreco = document.querySelector(".js-total-do-valor")
+var selecionarTodos = document.querySelector("js-selecionar-todos")
+var botaoConfirmar = document.querySelector(".js-botao-confirmar");
+
+
+var quantidadeCursos = 0;
+var quantidadeHoras = 0;
+var somaPreco = 0;
+
+function adicionarCurso(checkbox){
+    if(checkbox.checked){
+        quantidadeCursos++;
+        quantidadeHoras += parseInt(checkbox.value);
+        somaPreco += parseFloat(checkbox.getAttribute('preco'));
+    } else{
+        quantidadeCursos--;
+        quantidadeHoras -= parseInt(checkbox.value);
+        somaPreco -= parseFloat(checkbox.getAttribute('preco'));
+    }
+    habilitarBotao();
+
+    totalCursos.textContent = quantidadeCursos + " curso(s)";
+    totalHoras.textContent = quantidadeHoras + "h";
+    totalPreco.textContent = "R$ " + somaPreco ;
+    
+}
+
+function habilitarBotao(){
+    if(quantidadeCursos > 0){
+        botaoConfirmar.classList.remove("disabled");
+    } else {
+        botaoConfirmar.classList.add("disabled");
+    }
+    
+}
+
+function adicionarTodos(){
+    
+    var cursos = document.getElementsByName("curso");
+    for (let index = 0; index < cursos.length; index++) {
+        console.log(cursos[index].checked);
+       cursos[index].checked=true;
+       
+        
+    }
+}
+
